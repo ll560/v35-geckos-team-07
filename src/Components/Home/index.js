f56af8751ec1f2a5f2696048547762b61f969591
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-11-15 09:36:34 -0500

    added responsiveness

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../App.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -12,40 +13,40 @@ function Home() {
                 </Col>
             </Row>
             <Row className="sign-row">
-                <Col className="sign-column" sm={6} lg={3}> 
+                <Col className="sign-column" sm={6} lg={4}> 
                     Sign 1
                 </Col>
-                <Col className="sign-column" sm={6} lg={3}>
-                    Sign
-                </Col> 2
-                <Col className="sign-column" sm={6} lg={3}>
-                    Sign 3
-                </Col>
-                <Col className="sign-column" sm={6} lg={3}>
+                <Col className="sign-column" sm={6} lg={4}>
+                    Sign 2
+                </Col> 
+                <Col className="sign-column" sm={6} lg={4}>
                     Sign 4
                 </Col>
-                <Col className="sign-column" sm={6} lg={3}>
+                <Col className="sign-column" sm={6} lg={4}>
+                    Sign 4
+                </Col>
+                <Col className="sign-column" sm={6} lg={4}>
                     Sign 5
                 </Col>
-                <Col className="sign-column" sm={6} lg={3}>
+                <Col className="sign-column" sm={6} lg={4}>
                     Sign 6
                 </Col>
-                <Col className="sign-column" sm={6} lg={3}>
+                <Col className="sign-column" sm={6} lg={4}>
                     Sign 7
                 </Col>
-                <Col className="sign-column" sm={6} lg={3}>
+                <Col className="sign-column" sm={6} lg={4}>
                     Sign 8
                 </Col>
-                <Col className="sign-column" sm={6} lg={3}>
+                <Col className="sign-column" sm={6} lg={4}>
                     Sign 9
                 </Col>
-                <Col className="sign-column" sm={6} lg={3}>
+                <Col className="sign-column" sm={6} lg={4}>
                     Sign 10
                 </Col>
-                <Col className="sign-column" sm={6} lg={3}>
+                <Col className="sign-column" sm={6} lg={4}>
                     Sign 11
                 </Col>
-                <Col className="sign-column" sm={6} lg={3}>
+                <Col className="sign-column" sm={6} lg={4}>
                     Sign 12
                 </Col>
             </Row>
