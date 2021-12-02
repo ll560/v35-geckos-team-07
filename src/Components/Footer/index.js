28c982cd771d275566eabee7a0ebad5dbf65f259
Ashley Christie <achristie0319@gmail.com>
2021-12-01 22:29:06 -0500

    style navbar and footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
 
 function Footer() {
-    return(
-        <div>
-            <h2>Footer</h2>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <p className="footer-love">Made with Love</p>
+      <span>&#128147;</span>
+      <Link to="/team" style={{ textDecoration: "none" }}>
+        <p className="footer-link">Meet the Developers</p>
+      </Link>
+    </div>
+  );
 }
 
 export default Footer;
